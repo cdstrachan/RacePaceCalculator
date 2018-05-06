19540a1e8b1e9a3178b477fececb9a5940aef59e
Craig Strachan <cstrachan@oldmutual.com>
2018-05-06 13:39:18 +0200

    updates

--- src/main/resources/static/js/pacecalculator.js
+++ src/main/resources/static/js/pacecalculator.js
@@ -17,7 +17,7 @@ app.controller('Pacecalculator', function($scope, $http) {
             $scope.loadingmessage = "Loading";
             $http.post('/pacechart', $scope.paceChartInput).then(function (response) {
                 $scope.paceChart = response.data;
-                $scope.loadingmessage = "";
+                $scope.loadingmessage = "Chart(s) created";
             }).catch(function(e){
                 console.log("Error creating chart",e);
                 $scope.CriticalErrorMessage = "Error connecting to server";
