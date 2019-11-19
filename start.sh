c1658706a97ae3a97e665db83772ebc0fc772877
Craig Strachan <cstrachan@oldmutual.com>
2019-11-19 13:26:17 +0200

    fixed shell script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ echo 'Starting Spring Boot app'
 cd '//home/ec2-user/target'
 
 chmod 500 paceservices-0.0.4.jar
+sudo service pacec stop
 unlink /etc/init.d/pacec
 sudo ln -s /home/ec2-user/target/paceservices-0.0.4.jar /etc/init.d/pacec
 sudo service pacec start
