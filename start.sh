78464ab0e9814b083a9c0b60be6084cfc51dbe1b
Craig Strachan <cstrachan@oldmutual.com>
2019-11-19 12:55:28 +0200

    update startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
-cd '//home/ec2-user'
-java -jar paceservices-0.0.4
+cd '//home/ec2-user/target'
+
+chmod 500 paceservices-0.0.4.jar
+unlink /etc/init.d/pacec
+sudo ln -s /home/ec2-user/target/paceservices-0.0.4.jar /etc/init.d/pacec
+sudo service pacec start
+Writes console logs to /var/log/<appname>.log
