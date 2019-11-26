bee98b78eb2cbd27387355db0358c0fbadb0b77a
Craig Strachan <cstrachan@oldmutual.com>
2019-11-26 21:33:26 +0200

    Fixed

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot app'
-cd '//home/ec2-user/pace/target'
 
-chmod 500 paceservices-0.0.4.jar
-sudo service pacec stop
-unlink /etc/init.d/pacec
-sudo ln -s /home/ec2-user/target/paceservices-0.0.4.jar /etc/init.d/pacec
+chmod 500 //home/ec2-user/pace/target/paceservices-0.0.4.jar
+sudo ln -s /home/ec2-user/pace/target/paceservices-0.0.4.jar /etc/init.d/pacec
 sudo service pacec start
--- stop.sh
+++ stop.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 echo 'Stopping Spring Boot app'
-cd '//home/ec2-user/'
-
 
 sudo service pacec stop
 unlink /etc/init.d/pacec
