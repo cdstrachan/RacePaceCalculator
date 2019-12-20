ea0dce231fa904c1ede4ca328feec39ff0718893
Craig Strachan <craig+aws@craigstrachan.com>
2019-12-20 09:19:08 +0200

    Updated data utils

--- src/main/java/com/cds/paceservices/DataUtils.java
+++ src/main/java/com/cds/paceservices/DataUtils.java
@@ -19,29 +19,37 @@ public class DataUtils {
 
     private static final Logger log = LoggerFactory.getLogger(PaceCalculatorController.class);
 
+    // utility function to create a log record
     public void writeRequestRecord(String requestData) {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(com.amazonaws.regions.Regions.US_EAST_2).build();
 
+        // connect to the database
         DynamoDB dynamoDB = new DynamoDB(client);
-
         Table table = dynamoDB.getTable("Audit");
 
+        // create a GUID
         String pkGUID = UUID.randomUUID().toString();
 
+        // dum the TO into a mappable object
         final Map<String, Object> infoMap = new HashMap<String, Object>();
-        // infoMap.put("timestamp", DateTime.now().toString());
         infoMap.put("requestPayload", requestData);
 
+        // save the log record
         try {
             log.info("Adding a new item...");
+            /*
+             * PutItemOutcome outcome = table.putItem(new Item().withPrimaryKey("pkGUID",
+             * pkGUID) .withString("timeStamp",
+             * DateTime.now().toString()).withMap("request", infoMap));
+             */
             PutItemOutcome outcome = table.putItem(new Item().withPrimaryKey("pkGUID", pkGUID)
-                    .withString("timeStamp", DateTime.now().toString()).withMap("request", infoMap));
+                    .withString("timeStamp", DateTime.now().toString()).withString("requestData", requestData));
 
-            log.info("PutItem succeeded:\n" + outcome.getPutItemResult());
+            log.info("Adding a new item...succeeded:" + outcome.getPutItemResult());
 
         } catch (Exception e) {
-            log.info("Unable to add item");
+            log.info("Unable to add item to dynamodb");
             log.info(e.getMessage());
         }
     }
