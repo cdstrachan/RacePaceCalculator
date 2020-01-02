ffb12077363d5463dbdd5f65b19897b4f84e9025
Craig Strachan <cstrachan@oldmutual.com>
2020-01-02 19:05:47 +0200

    Updates

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo 'Starting Spring Boot app'
+echo 'Starting Spring Boot app version 0.0.5'
 
 chmod 500 //home/ec2-user/pace/target/paceservices-0.0.5.jar
 sudo ln -s /home/ec2-user/pace/target/paceservices-0.0.5.jar /etc/init.d/pacec
